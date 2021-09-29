b1910cad69a52621260850de66354a90d4fdff33
robzard <pal-vitek1111@mail.ru>
2021-09-29 13:40:22 +0500

    make it better

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
-# TOKEN = '1998159927:AAGeMUGD7U0EzVvopx09uY6g-l9S0BJhEgc'
-# ADMINS = [521793004, 601610220, 195895280]
-# HOST = 'localhost'
-# PG_USER = 'postgres'
-# PG_PASS = '781636118'
-# PORT = '5432'
-# DATABASE = 'telegrambott'
-
 TOKEN = '1998159927:AAGeMUGD7U0EzVvopx09uY6g-l9S0BJhEgc'
 ADMINS = [521793004, 601610220, 195895280]
-HOST = 'ec2-54-228-99-58.eu-west-1.compute.amazonaws.com'
-PG_USER = 'gvlxymybblkpuk'
-PG_PASS = 'cbab39e99ae1cb33f11f94af5cbf3ed9e42100618396f6df8d63969012452ddc'
+HOST = 'localhost'
+PG_USER = 'postgres'
+PG_PASS = '781636118'
 PORT = '5432'
-DATABASE = 'd6rpbns39salr'
+DATABASE = 'telegrambott'
+
+# TOKEN = '1998159927:AAGeMUGD7U0EzVvopx09uY6g-l9S0BJhEgc'
+# ADMINS = [521793004, 601610220, 195895280]
+# HOST = 'ec2-54-228-99-58.eu-west-1.compute.amazonaws.com'
+# PG_USER = 'gvlxymybblkpuk'
+# PG_PASS = 'cbab39e99ae1cb33f11f94af5cbf3ed9e42100618396f6df8d63969012452ddc'
+# PORT = '5432'
+# DATABASE = 'd6rpbns39salr'
